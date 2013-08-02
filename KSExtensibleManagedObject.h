4c59a781d9cfa71e214a36010063c0d5f5922878
Mike Abdullah <me@mikeabdullah.net>
2013-08-02 16:27:46 +0100

    Appledoc

--- KSExtensibleManagedObject.h
+++ KSExtensibleManagedObject.h
@@ -39,51 +39,111 @@
 
 #pragma mark Extensible Properties
 
+/**
+ Retrieves a value from the extensible properties store.
+ 
+ @param key The key to lookup. Must not be `nil`.
+ @result The value corresponding to `key`. Might be `nil`.
+ */
 - (id)extensiblePropertyForKey:(NSString *)key;
+
+/**
+ Retrieves a copy of the extensible properties store.
+ 
+ @result All the extensible properties currently stored.
+ */
 - (NSDictionary *)extensibleProperties;
 
-- (void)setExtensibleProperty:(id)value forKey:(NSString *)key; // value should be non-nil
+/**
+ Stores an extensible property.
+ 
+ @param value The value to store. Should not be `nil`.
+ @param key The key to store. Must not be `nil`.
+ */
+- (void)setExtensibleProperty:(id)value forKey:(NSString *)key;
+
+/**
+ Removes an extensible property.
+ 
+ @param key The key to remove from the extensible properties store. Must not be `nil`.
+ */
 - (void)removeExtensiblePropertyForKey:(NSString *)key;
 
-// Called after undoing or redoing change to a key. Default implementation does nothing, but should call super first when overriding
+/**
+ Invoked automatically when an extensible properties is reset due to an undo or redo state change.
+ 
+ Default implementation does nothing, but should call `super` first when
+ overriding for future compatibility.
+ 
+ @param key The key for whose values has changed.
+ */
 - (void)awakeFromExtensiblePropertyUndoUpdateForKey:(NSString *)key;
 
 
 #pragma mark KVC/KVO Integration
 
-/*!
- @method usesExtensiblePropertiesForUndefinedKey:
- @abstract Template method to let extensible properties be directly accessible from Key-Value Coding
- @param key The key being queried
- @result Default implementation returns NO for all keys
- @discussion Whenever a method such as -valueForUndefinedKey: or -setValue:forUndefinedKey: is called, the receiver will call this method to define the behaviour. If you return YES, the value will be stored/retrieved in/from extensible properties, and appropriate KVO notifications posted. If not, super's implementation will be invoked (i.e. raise an NSUndefinedKeyException)
+/**
+ Whether a given extensible property should be made KVC and KVO compliant.
+ 
+ Whenever a method such as `-valueForUndefinedKey:` or
+ `-setValue:forUndefinedKey:` is called, the receiver will call this method to
+ define the behaviour. If you return `YES`, the value will be stored/retrieved
+ in/from extensible properties, and appropriate KVO notifications posted. If not,
+ `super`'s implementation will be invoked (i.e. `NSUndefinedKeyException` is
+ raised).
+ 
+ @param key The key being queried.
+ @result Default implementation returns `NO` for all keys.
  */
 - (BOOL)usesExtensiblePropertiesForUndefinedKey:(NSString *)key;
 
 
 #pragma mark Core Data Integration
 
-/*	Takes the standard -changedValues and -committedValuesForKeys: methods and adds support for
- *	KSExtensibleManagedObject.
+/**
+ An extension to `-[NSManagedObject committedValuesForKeys:]` that can include extensible properties too.
+ 
+ @param keys The keys to retrieve. Pass `nil` to retrieve all keys/values.
+ @param flag Whether to include extensible properties in the list of changed values.
+ @result The values last committed to disk.
  */
 - (NSDictionary *)committedValuesForKeys:(NSArray *)keys includeExtensibleProperties:(BOOL)flag;
+
+/**
+ An extension to `-[NSManagedObject changedValues]` that can include extensible properties too.
+ 
+ @param flag Whether to include extensible properties in the list of changed values.
+ @result The changed values.
+ */
 - (NSDictionary *)changedValuesIncludingExtensibleProperties:(BOOL)flag;
 
 
 #pragma mark Extensible Property Storage
 
-/*!	These two methods are called by KSExtensibleManagedObject when archiving or unarchiving
- *	the dictionary it uses in-memory. You can override them in a subclass to tweak the
- *	behaviour. e.g. To use an encoding method other than NSKeyedArchiver.
+/**
+ Unarchives extensible properties.
+ 
+ Subclasses can override to perform their own custom unarchiving.
+ 
+ @param propertiesData The serialized extensible properties.
+ @result The deserialized extensible properties.
  */
 + (NSDictionary *)unarchiveExtensibleProperties:(NSData *)propertiesData;
+
+/**
+ Archives extensible properties.
+ 
+ Subclasses can override to perform their own custom archiving.
+ 
+ @param properties The extensible properties.
+ @result Deserialized representation of the extensible properties.
+ */
 + (NSData *)archiveExtensibleProperties:(NSDictionary *)properties;
 
-/*!
- @method extensiblePropertiesDataKey
- @abstract Template method
- @result The key under which to store the archived representation of the extensible properties. By default this is "extensiblePropertiesData"
- @discussion Override this method to store extensible properties using a different key.
+/**
+ Override this method to store extensible properties using a different key.
+ 
+ @result The key under which to store the archived representation of the extensible properties. By default this is `extensiblePropertiesData`.
  */
 + (NSString *)extensiblePropertiesDataKey;
 
